bf7f0eb7a875fc859551400f2d44accd3413316d
GT Fari <guideg6@gmail.com>
2020-11-02 21:57:31 +0200

    Redux Into HomeScreen - useDispatch & useSelector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,36 @@
+import {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import Product from '../components/Product'
-import {useState, useEffect} from 'react'
-import axios from 'axios'
+import {listProducts} from '../actions/productActions'
+import { productListReducer } from '../reducers/productReducers'
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {loading, error, products} = productList
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const {data} = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, []) 
+        dispatch(listProducts())
+    }, [dispatch]) 
+
 
     return (
         <>
             <h1 className='mb-5 text-2xl font-bold'>Latest products</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                {products.map(product => 
-                    <div>
-                        <Product product={product}/>
-                    </div>
-                    )}
-            </div>
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <h3>{error}</h3>
+            ) : ( 
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                    {products.map(product => 
+                        <div>
+                            <Product product={product}/>
+                        </div>
+                        )}
+                </div>
+            )
+            }
         </>
     )
 }
