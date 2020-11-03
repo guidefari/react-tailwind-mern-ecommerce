9933e6fe964fc4ea0330db5d81e971e5451a47e2
GT Fari <guideg6@gmail.com>
2020-11-03 12:13:29 +0200

    cart reducer & add to cart action

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,7 +1,7 @@
 const Loader = () => {
     return (
-        <div class="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
-            <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        <div className="fixed top-0 right-0 z-50 flex items-center justify-center w-screen h-screen">
+            <div className="w-32 h-32 border-t-2 border-b-2 border-gray-900 rounded-full animate-spin"></div>
         </div>
 )
 }
