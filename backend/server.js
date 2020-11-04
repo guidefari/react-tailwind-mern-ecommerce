9f53bbb94abba25cd1fec112355895cee7d5ee31
GT Fari <guideg6@gmail.com>
2020-11-04 11:16:20 +0200

    bug dead

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ import dotenv from 'dotenv'
 import { notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
-import productRoutes from './Routes/productRoutes.js'
-import userRoutes from './Routes/userRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 connectDB()
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
 
 app.get('/', (req,res) => {
     res.send('API is running')
