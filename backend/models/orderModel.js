796e7bb581cfb6fce65469e7acb76b5e4825c157
GT Fari <guideg6@gmail.com>
2020-11-07 14:23:49 +0200

    orderModel update

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -19,8 +19,7 @@ const orderSchema = mongoose.Schema({
     shippingAddress: {
         address: { type: String, required: true},
         city: { type: String, required: true},
-        postalCode: { type: String, required: true},
-        country: { type: String, required: true},
+        suburb: { type: String, required: true},
     },
     paymentMethod: {
         type: String,
